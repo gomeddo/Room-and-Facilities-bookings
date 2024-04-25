77dd92fdb77a5fb93784689f903da2f4a43aab12
Rizel Thomas <r.thomas@gomeddo.com>
2024-04-25 14:50:10 +0200

    Make room card features span and not button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,36 +8,36 @@ function Dashboard() {
       title: "Full Furnished Smart Studio Apartments",
       image: room,
       alt: "product image",
-      features: ["2 guests", "Free Parking", "Wifi"],
+      features: ["2 guests", "Bicycle storage", "Free Wi-Fi"],
     },
     {
       title: "Full Furnished Smart Studio Apartments",
       image: room,
       alt: "product image",
-      features: ["2 guests", "Free Parking", "Wifi"],
+      features: ["1 guests", "Heating and/or air conditioning", "Free Wi-Fi"],
     },
     {
       title: "Full Furnished Smart Studio Apartments",
       image: room,
       alt: "product image",
-      features: ["2 guests", "Free Parking", "Wifi"],
+      features: ["2 guests", "Laundry facilities", "Free Wi-Fi"],
     },
     {
       title: "Full Furnished Smart Studio Apartments",
       image: room,
       alt: "product image",
-      features: ["2 guests", "Free Parking", "Wifi"],
+      features: ["2 guests", "Free Parking", "Free Wi-Fi"],
     },
     {
       title: "Full Furnished Smart Studio Apartments",
       image: room,
       alt: "product image",
-      features: ["2 guests", "Free Parking", "Wifi"],
+      features: ["4 guests", "Pool", "Free Wi-Fi", "Gym"],
     },
     // Add more card data objects as needed
   ];
   return (
-    <div className="flex p-6 bg-slate-400 max-h-screen" style={{ backgroundColor: '#EEEEEE', paddingTop: '40px', paddingBottom: '40px' }}>
+    <div className="flex p-6 bg-gray-200 max-h-screen pt-10 pb-10">
       {/* Left portion taking 1/4 of the page */}
       <div className="w-1/4 bg-white shadow-lg rounded p-3">
         {/* Date Picker */}
@@ -55,43 +55,67 @@ function Dashboard() {
             <option value="">Option {index + 1}</option>
           </select>
         ))}
+
+        {/* Apply and Cancel Buttons */}
+        <div className="flex justify-end mx-1">
+          <button className="bg-blue-500 text-white p-2 rounded-md mb-2 mr-2">
+            Apply
+          </button>
+          <button className="bg-gray-500 text-white p-2 rounded-md mb-2 mr-2">
+            Cancel
+          </button>
+        </div>
       </div>
 
       {/* Right portion taking 3/4 of the page */}
       <div className="w-3/4  p-6">
-      {cardsData.map((card, index) => (
-        <div key={index} className="w-full mb-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex">
-          {/* Image */}
-          <a href="#">
-            <img
-              className="p-4 rounded-lg w-72 h-52 object-cover"
-              src={card.image}
-              alt={card.alt}
-            />
-          </a>
-          {/* Product Information */}
-          <div className="flex flex-col  px-5 my-2 w-full">
-            <h5 className="text-xl py-2 font-semibold tracking-tight text-gray-900 dark:text-white font-ubuntu text-left">
-              {card.title}
-            </h5>
-            <div className="flex items-center py-2">
-              <img src={star} className="w-6 h-6" alt="" />
-              <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-                Mercedes Vito
-              </span>
+        {cardsData.map((card, index) => (
+          <div
+            key={index}
+            className="w-full mb-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex"
+          >
+            {/* Image */}
+            <a href="#">
+              <img
+                className="p-4 rounded-lg w-72 h-52 object-cover"
+                src={card.image}
+                alt={card.alt}
+              />
+            </a>
+            {/* Product Information */}
+            <div className="flex flex-col  px-5 my-2 w-full">
+              <h5 className="text-xl py-2 font-semibold tracking-tight text-gray-900 dark:text-white font-ubuntu text-left">
+                {card.title}
+              </h5>
+              <div className="flex items-center py-2">
+                <img src={star} className="w-6 h-6" alt="" />
+                <span className="text-solid-neutral-700 text-xs font-semibold px-2.5 py-0.5 rounded dark:text-solid-neutral-700 ms-3">
+                  Mercedes Vito
+                </span>
+              </div>
+              <div className="flex items-center space-x-2 my-2 ">
+                {/* Features */}
+                {card.features.map((feature, index) => (
+                  <span key={index} className="bg-violet-100 rounded-full p-2">
+                    {feature}
+                  </span>
+                ))}
+              </div>
             </div>
-            <div className="flex items-center space-x-2 my-2 ">
-              {/* Buttons */}
-              {card.features.map((feature, index) => (
-                <button key={index} className="bg-slate-300 rounded-full p-2">
-                  {feature}
-                </button>
-              ))}
+
+            {/* Verify Button */}
+            <div className="flex flex-col justify-center m-2 items-center space-y-2">
+              <button className="text-black bg-white shadow-lg px-4 py-2 rounded-lg">
+                Verify
+              </button>
+              {/* Cancel Button */}
+              <button className=" bg-blue-900 hover:bg-blue-800 text-white shadow-lg focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Cancel
+              </button>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <nav className="bg-white px-8 p-4 flex shadow-md flex-col md:flex-row md:justify-between items-center" style={{ fontSize: '24px' }}>
-      {/* Text on the left */}
+    <nav className="bg-white px-8 p-4 flex shadow-md flex-col md:flex-row md:justify-between items-center text-2xl">
+      {/* <!-- Text on the left --> */}
       <div className="text-black font-bold mb-4 md:mb-0 md:mr-4">Amsterdam Metropolitan University</div>
-      {/* Nav Links on the right */}
+
+      {/* <!-- Nav Links on the right --> */}
       <div className="flex flex-wrap">
-        <Link to="/cities" className="text-black mr-4 mb-2 md:mb-0 font-medium" style={{ fontSize: '14px' }}>Cities</Link>
-        <Link to="/about" className="text-black mr-4 mb-2 md:mb-0 font-medium" style={{ fontSize: '14px' }}>Room</Link>
-        <Link to="/contact" className="text-black mb-2 md:mb-0 font-medium" style={{ fontSize: '14px' }}>Contact us</Link>
+        <Link to="/" className="text-black mr-4 mb-2 md:mb-0 font-medium text-sm">Home</Link>
+        <Link to="/about" className="text-black mr-4 mb-2 md:mb-0 font-medium text-sm">Room</Link>
+        <Link to="/contact" className="text-black mb-2 md:mb-0 font-medium text-sm">Contact us{" "}</Link>
       </div>
     </nav>
   );
