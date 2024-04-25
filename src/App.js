771b74b284cba17edb37674061bb5f7e8a710c69
Rizel Thomas <r.thomas@gomeddo.com>
2024-04-25 14:50:39 +0200

    Added header and updated dashboard name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './assets/logo.svg';
-import './App.css';
 import Header from './components/Header';
-import Home from './components/Dashboard';
+import Dashboard from './components/Dashboard';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-     <Header/>
+      <Header/>
      <Dashboard/>
      </BrowserRouter>
     </div>
