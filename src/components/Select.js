1a0c3fd449f7ddcb06a82ee2b42c8ab02cd4a442
Rizel Thomas <r.thomas@gomeddo.com>
2024-05-02 12:30:21 +0200

    Added violet Feather ChevronDown icon to Select component

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,14 +1,19 @@
-import { twMerge } from "tailwind-merge";
+import React from "react";
+import { ChevronDown } from "react-feather";
 
 export default function Select(props) {
   const { className, ...rest } = props;
   return (
-    <select
-      {...rest}
-      className={twMerge(
-        "w-full bg-[#EEEEEE] px-3 py-2.5 rounded-lg text-[#444444] text-sm",
-        className
-      )}
-    />
+    <div className="relative">
+      <select
+        {...rest}
+        className={`appearance-none w-full rounded-lg px-3 py-2.5 pl-3 pr-10 bg-[#EEEEEE] text-gray-700 text-sm ${className}`}
+      >
+        {props.children}
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-violet-900">
+        <ChevronDown className="h-6 w-6" />
+      </div>
+    </div>
   );
 }
