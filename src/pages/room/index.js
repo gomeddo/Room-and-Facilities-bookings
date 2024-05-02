df20b929ff45b0e7bff70f026199b4acfaf99e8e
Rizel Thomas <r.thomas@gomeddo.com>
2024-05-02 15:15:00 +0200

    Added image slider

--- src/pages/room/index.js
+++ src/pages/room/index.js
@@ -1,13 +1,21 @@
-import { X } from "react-feather";
-import { Star } from "react-feather";
+import { ChevronLeft, ChevronRight, Star, X } from "react-feather";
 import { useNavigate, useParams } from "react-router-dom";
 import Chip from "../../components/chip";
 import { cardsData } from "../../constants";
+import apart1 from "../../assets/apart_1.jpg";
+import apart2 from "../../assets/apart_2.jpg";
+import apart3 from "../../assets/apart_3.jpg";
+import { useState } from "react";
+
+const photos = [apart1, apart2, apart3];
 
 export default function RoomPage() {
   const { roomId } = useParams();
+
+  const [selectedPhoto, setSelectedPhoto] = useState(0);
+
   const room = cardsData[roomId];
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleClose = () => {
     navigate("/"); // Navigate back to the home page or any other desired route
@@ -42,35 +50,40 @@ const navigate = useNavigate();
           <X className="w-18 h-18" />
         </div>
         <div className="max-w-7xl p-20 h-full">
-          <div className="w-full pb-8">
+          <div className="w-full pb-8 relative">
+            <ChevronLeft
+              onClick={() => {
+                setSelectedPhoto((state) =>
+                  state === 0 ? photos.length - 1 : state - 1
+                );
+              }}
+              className="absolute top-[calc(50%-16px)] left-4 p-2 w-8 h-8 bg-white rounded-full opacity-75 hover:opacity-100 hover:scale-105 transition-all cursor-pointer"
+            />
+            <ChevronRight
+              onClick={() =>
+                setSelectedPhoto((state) =>
+                  state === photos.length - 1 ? 0 : state + 1
+                )
+              }
+              className="absolute top-[calc(50%-16px)] right-4 p-2 w-8 h-8 bg-white rounded-full opacity-75 hover:opacity-100 hover:scale-105 transition-all cursor-pointer"
+            />
             <img
-              src={room.image}
+              src={photos[selectedPhoto]}
               alt={room.alt}
-              className="rounded-lg object-cover w-full h-auto"
+              className="rounded-lg object-cover w-full h-auto aspect-video select-none"
             />
           </div>
           <div className="flex flex-row gap-8 w-full">
-            <div className="flex-1">
-              <img
-                src={room.image}
-                alt={room.alt}
-                className="rounded-lg object-cover"
-              />
-            </div>
-            <div className="flex-1">
-              <img
-                src={room.image}
-                alt={room.alt}
-                className="rounded-lg object-cover"
-              />
-            </div>
-            <div className="flex-1">
-              <img
-                src={room.image}
-                alt={room.alt}
-                className="rounded-lg object-cover"
-              />
-            </div>
+            {photos.map((photo, index) => (
+              <div className="flex-1 cursor-pointer hover:scale-105 transition-all">
+                <img
+                  src={photo}
+                  alt={room.alt}
+                  className="rounded-lg object-cover aspect-video"
+                  onClick={() => setSelectedPhoto(index)}
+                />
+              </div>
+            ))}
           </div>
         </div>
       </div>
