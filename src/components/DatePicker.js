a180957acc72ad3d51cf5c6f8d3e7d85534c3b13
Rizel Thomas <r.thomas@gomeddo.com>
2024-05-02 13:43:21 +0200

    Added calendar icon to DatePicker

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,6 +1,7 @@
 import { Popover, PopoverTrigger, PopoverContent } from "./popover";
 import { Calendar } from "./calendar";
 import { useState } from "react";
+import { Calendar as CalendarIcon } from "react-feather";
 
 export default function DatePicker(props) {
   const [date, setDate] = useState(undefined);
@@ -9,10 +10,11 @@ export default function DatePicker(props) {
   return (
     <Popover>
       <PopoverTrigger>
-        <div className="w-full text-start bg-[#EEEEEE] px-3 py-2.5 rounded-lg text-[#444444] text-sm">
+        <div className="flex items-center justify-between w-full text-start bg-[#EEEEEE] px-2.5 py-2.5 rounded-lg text-[#444444] text-sm">
           {!!date?.from ? date.from.toDateString() : "From"}
           {" - "}
           {!!date?.to ? date.to.toDateString() : "To"}
+          <CalendarIcon className="h-4.1 w-4. text-violet-900" />
         </div>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0 bg-white">
