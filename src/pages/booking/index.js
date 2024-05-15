d63955d34dfc9d8b26e1318c0d4902698005c11e
Rizel Thomas <r.thomas@gomeddo.com>
2024-05-15 14:30:18 +0200

    Storing user input and display errors for missing inputs
    Storing user input as a new contact onto the new reservation and display errors for missing inputs

--- src/pages/booking/index.js
+++ src/pages/booking/index.js
@@ -7,12 +7,19 @@ import clsx from "clsx"; // Importing clsx library for conditional classes
 import { useRoomContext } from "../../context"; // Context hook for room data
 import useGoMeddo from "../../hooks/useGoMeddo"; // Custom hook for GoMeddo API
 import { Contact, Reservation } from "@gomeddo/sdk"; // SDK for GoMeddo service
+import { useState } from "react"; // Importing useState hook for managing form state
 
 // Functional component for booking details
 function Booking(props) {
   const { rooms, duration } = useRoomContext();
   const [, setSearchParams] = useSearchParams(); // Getting and setting URL search parameters
   const gm = useGoMeddo();
+
+  // State variables for form fields
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   const room = rooms.at(props.id);
   if (!room) {
@@ -51,23 +58,43 @@ function Booking(props) {
         {/* Labels and Inputs for user information, disabled if dates not selected */}
         <Label className="flex flex-col">
           First Name
-          <Input disabled={!areDatesSelected} />
+          <Input
+            disabled={!areDatesSelected}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required // Making the input required
+          />
         </Label>
         <Label className="flex flex-col">
           Last Name
-          <Input disabled={!areDatesSelected} />
+          <Input
+            disabled={!areDatesSelected}
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required // Making the input required
+          />
         </Label>
         <Label className="flex flex-col">
           Email
-          <Input disabled={!areDatesSelected} />
+          <Input
+            disabled={!areDatesSelected}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required // Making the input required
+          />
         </Label>
         <Label className="flex flex-col">
           Phone Number
-          <Input disabled={!areDatesSelected} />
+          <Input
+            disabled={!areDatesSelected}
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            required // Making the input required
+          />
         </Label>
         {/* Checkbox for data permission, disabled if dates not selected */}
         <div className="flex gap-4 text-xs pt-2">
-          <input type="checkbox" disabled={!areDatesSelected} />
+          <input type="checkbox" disabled={!areDatesSelected} required />
           <div>
             I give permission to save the data I have entered here and use this
             data to contact me. More information in our privacy statement.
@@ -90,7 +117,10 @@ function Booking(props) {
             disabled={!areDatesSelected}
             onClick={async () => {
               const reservation = new Reservation()
-                .setContact(new Contact("Test", "Test", "test@test.com"))
+                // Using user input to set contact information
+                .setContact(
+                  new Contact(firstName, lastName, email, phoneNumber)
+                )
                 .setResource(room)
                 .setStartDatetime(new Date(duration.from))
                 .setEndDatetime(new Date(duration.to));
